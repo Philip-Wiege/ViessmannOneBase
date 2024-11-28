90e6fca073f00cf9b8e930996c51751ec10bed8b
Philip Wiege <Philip.Wiege@googlemail.com>
2024-11-28 22:36:43 +0100

    Update __init__.py

--- custom_components/viessmann_onebase_local/__init__.py
+++ custom_components/viessmann_onebase_local/__init__.py
@@ -3,11 +3,11 @@ from __future__ import annotations
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.typing import ConfigType
 
-DOMAIN = "viessmann_onebase_local"
+DOMAIN = "viessmann_onebase"
 
 
 def setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    hass.states.set('viessmann_onebase_local.Hello_World', 'Test String')
-    hass.states.set('viessmann_onebase_local.Vitocal_250A.FlowTemperature', '25.00')
+    hass.states.set('viessmann_onebase.Hello_World', 'Test String')
+    hass.states.set('viessmann_onebase.Vitocal_250A.FlowTemperature', '25.00')
 
     return True
